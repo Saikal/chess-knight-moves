6e2be7dd994a12b28e3cd47061eb461ffd29e6af
Saikal <saikal@github.com>
2020-06-10 16:15:20 -0700

    API Post request to post knight position in Postgres DB in Heroku and calculation of Knight Move I and Move II, wih Error handling and checking if request data is Algebraic

--- server/index.js
+++ server/index.js
@@ -18,6 +18,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 
+app.post('/move', async (req, res) => {
+
+  try {
+    let convertedAlgebaic = Handler.convertFromAlgebraic(req.body['location']);
+    if (!convertedAlgebaic) {
+      throw new Error('Not Algebraic');
+    } else {
+      /* 
+        START DB Postgres POST request knight:
+
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO knight(user, move) VALUES(' + `'${req.body['user']}'` + ',' + `'${req.body['location']}'`);
+      */
+      let results = {};
+
+      let firstMove = Handler.knightMoves(convertedAlgebaic);
+      let firstSet = new Set();
+      Handler.populateSet(firstSet,  firstMove);
+
+      results['moves_first'] = Array.from(firstSet);
+      let secondSet = new Set();
+      for (const pos of firstMove) { Handler.populateSet(secondSet, Handler.knightMoves(pos)); }
+      results['moves_second'] = Array.from(secondSet);
+
+      res.send(results);
+      /*
+        END DB Postgres POST request knight:
+
+        client.release();
+      */
+    }
+  } catch (err) {
+    res.send(null);
+    throw err;
+  }
+});
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'../client/public/index.html'));
 });
