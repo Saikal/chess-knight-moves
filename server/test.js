e2ebb23827309cf4d1caae3f2121472f04dd8f82
Saikal <saikal@github.com>
2020-06-10 16:24:15 -0700

    Test API request body, has to be valid Algebraic data

--- server/test.js
+++ server/test.js
@@ -2,6 +2,22 @@
 
 const handlers = require('./handlers.js');
 
+test('Test bad request for knight moves with NOT Algebraic req body 55: ', () => {
+  expect(handlers.convertFromAlgebraic('55')).toEqual(null);
+});
+
+test('Test bad request for knight moves with NOT Algebraic req body aa: ', () => {
+  expect(handlers.convertFromAlgebraic('aa')).toEqual(null);
+});
+
+test('Test bad request for knight moves with NOT Algebraic req body long string: ', () => {
+  expect(handlers.convertFromAlgebraic('gtfj5')).toEqual(null);
+});
+
+test('Test Converting from Algebraic: ', () => {
+  expect(handlers.convertFromAlgebraic('a8')).toEqual({ 'row': 0, 'col': 0 });
+});
+
 test('Valid Board cell Test: ', () => {
   expect(handlers.ifValid(5)).toEqual(true);
 });
