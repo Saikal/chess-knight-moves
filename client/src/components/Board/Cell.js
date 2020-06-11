905f3a3bc82336d5aceb91b491091e0e6ebf4d4a
Saikal <saikal@github.com>
2020-06-10 17:51:04 -0700

    Board cells build

--- client/src/components/Board/Cell.js
+++ client/src/components/Board/Cell.js
@@ -3,11 +3,12 @@ import React from 'react';
 class Cell extends React.Component {
 
   onClick (e) {
+    this.props.selectSpot(e.target.dataset.location);
   }
 
   render() {
     return (
-      <div>
+      <div ref={(node) => { this.cell = node; }} className={this.props.color + ' grid-item'}  data-location={this.props.location} id={this.props.idx} onClick={this.onClick.bind(this)} >
       </div>
     )
   }
