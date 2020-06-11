4b6a5405f1c69fda713e7e3665720784ccd7e75d
Saikal <saikal@github.com>
2020-06-10 17:49:33 -0700

    Board build

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -4,7 +4,21 @@ import Cell from './Cell.js';
 class Board extends React.Component {
   render() {
     return (
-      <div id="board">
+      <div id="board" ref={this.props.boardRef}>
+        <div className="chess-board">
+          <div className="horizontal-algebraic grid-container"><div></div><div>a</div><div>b</div><div>c</div><div>d</div><div>e</div><div>f</div><div>g</div><div>h</div><div></div></div>
+
+          { this.props.board.map((rows, rowIndex) => 
+              <div className="grid-container" key={rowIndex} >
+                <div className="vertical-algebraic">{8-rowIndex}</div>
+                { rows.map((cols, colIndex) => <Cell color={cols.cell} location={cols.loc} ref={(node) => { this[`${cols.loc}`] = node; }} selectSpot={this.props.refSetKnight} key={cols.loc} idx={rowIndex.toString() + colIndex.toString()} />) }
+                <div className="vertical-algebraic">{8-rowIndex}</div>
+              </div>
+            )
+          }
+
+          <div className="horizontal-algebraic grid-container"><div></div><div>a</div><div>b</div><div>c</div><div>d</div><div>e</div><div>f</div><div>g</div><div>h</div><div></div></div>        
+        </div>
       </div>     
     )
   }
